f7914a587146ad6e859ef6127a4c9e7dae561d67
Kevin Jones <kejones@redhat.com>
2017-10-07 21:57:34 -0400

    Config changes. Move brb baseline files into public
    Signed-off-by: Kevin Jones <kejones@redhat.com>

--- config.php
+++ config.php
@@ -3,25 +3,25 @@
 $appConf = \com\sgtinc\ApplicationConfig::getInstance();
 
 $appConf->__set('environment','production');
-$appConf->__set('title','Slim Skeleton');
+$appConf->__set('title','Big Red Button');
 $appConf->__set('uriRoot','/');
 
 //TODO: Should the DB Connection Info be configurable on init and within admin?
-$appConf->__set('dbHost','localhost');
-$appConf->__set('dbPort','3306');
-$appConf->__set('dbName','skeleton');
-$appConf->__set('dbUsername','skeleton');
-$appConf->__set('dbPassword','...');
+//$appConf->__set('dbHost','localhost');
+//$appConf->__set('dbPort','3306');
+//$appConf->__set('dbName','skeleton');
+//$appConf->__set('dbUsername','skeleton');
+//$appConf->__set('dbPassword','...');
 
 //TODO: Make LDAP configurable in the application
-$appConf->__set('ldapHost','ldap://localhost');
-$appConf->__set('ldapPort','636');
+//$appConf->__set('ldapHost','ldap://localhost');
+//$appConf->__set('ldapPort','636');
 
 //TODO: Make SMTP configurable in the application
-$appConf->__set('smtpHost','localhost');
-$appConf->__set('smtpPort','25');
-$appConf->__set('smtpFromEmail','no-reply@sgt-inc.com');
-$appConf->__set('smtpFromName','SGT Slim Skeleton');
+//$appConf->__set('smtpHost','localhost');
+//$appConf->__set('smtpPort','25');
+//$appConf->__set('smtpFromEmail','no-reply@sgt-inc.com');
+//$appConf->__set('smtpFromName','SGT Slim Skeleton');
 
 switch($_SERVER['SERVER_NAME']) {
 	case "localhost":
@@ -39,6 +39,6 @@ switch($_SERVER['SERVER_NAME']) {
 }
 
 //Instantiate MySQL DB connection
-$appConf->__set('dbConnString','mysql:host=' . $appConf->__get('dbHost') . ';port=' . $appConf->__get('dbPort') . ';dbname=' . $appConf->__get('dbName'));
+//$appConf->__set('dbConnString','mysql:host=' . $appConf->__get('dbHost') . ';port=' . $appConf->__get('dbPort') . ';dbname=' . $appConf->__get('dbName'));
 
 $GLOBALS['appConf'] = $appConf;
