b44d3a9e300ab416c2604381391b918103230980
Kevin Jones <kejones@redhat.com>
2017-10-07 22:24:09 -0400

    Move index.html into slim framework
    Signed-off-by: Kevin Jones <kejones@redhat.com>

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,25 @@
 //NOTE: Only functionality that is application wide should be applied here.
 $(document).ready(function() {
+	var stdout = []
+
+	$("#button-chaos").click(function() {
+		$(this).hide();
+		$("#img-dk-gif").removeClass("d-none").addClass("d-block").show();
+
+		stdout.push("<li>I have summoned the Human Chaos Monkey!!!!</li>");
+		stdout.push("<li>His job is to destroy a random node in your OCP cluster</li>");
+		stdout.push("<li>Prepare for destruction!!!!</li>");
+
+		console.log(stdout)
+	});
+
+	setInterval(function() {
+		console.log(stdout);
+		writeStdOut();
+	}, 3000);
+
+	function writeStdOut() {
+ 		$("#div-app-console").append(stdout.shift());
+	}
 
 });
